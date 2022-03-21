b89270f7449d4cd56394e6933a0c69bcd3809164
Mihail Voevodin <mrfirst3333@gmail.com>
2022-03-21 23:43:49 +0300

    add adaptive for header and fixes

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -13,10 +13,28 @@ function menu() {
     const menu = document.querySelector('.header__top');
 
     window.addEventListener('scroll', () => {
-        if (window.pageYOffset > '100') {
+        if (window.pageYOffset > '50') {
             menu.style.backgroundColor = "#b00";
         } else {
             menu.style.backgroundColor = "transparent";
+        }
+    })
+
+    const menuClose = document.querySelector('.hamburger-menu__close'),
+          menuSide = document.querySelector('.hamburger-menu'),
+          menuOpen = document.querySelector('.hamburger');
+
+    menuOpen.addEventListener('click', () => {
+        menuSide.classList.add('active');
+    }) 
+
+    menuClose.addEventListener('click', () => {
+        menuSide.classList.remove('active');
+    })
+
+    document.addEventListener('click', e => {
+        if (!e.target.closest('.hamburger-menu') && !e.target.closest('.hamburger')) {
+            menuSide.classList.remove('active');
         }
     })
 }
